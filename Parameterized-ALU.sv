411971ad2aa7cd2a325e1e5b311fed3bba350255
Orrin Dahanaggamaarachchi <107053915+123orrin@users.noreply.github.com>
2022-10-22 22:47:06 -0400

    Create Parameterized-ALU.sv

---
+++ Parameterized-ALU.sv
@@ -0,0 +1,21 @@
+// Implementation of a simple Parameterized Arithmetic Logic Unit
+
+`timescale 1ns/1ns
+
+module part3(A, B, Function, ALUout);
+	parameter N = 4;
+	input logic [N-1:0] A, B;
+	input logic [2:0] Function;
+	output logic [2*N-1:0] ALUout;
+
+	always_comb
+	begin
+	case (Function)
+		0: ALUout = A+B;
+		1: ALUout = |{A, B};
+		2: ALUout = &{A, B};
+		3: ALUout = {A, B};
+		default: ALUout = 1'b0;
+	endcase
+	end
+endmodule
